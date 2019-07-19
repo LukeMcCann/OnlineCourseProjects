35d564b09d72dc984c80ad7d388def557fbbde95
LukeMcCann <developer-lukemccann@outlook.com>
2019-07-19 15:56:18 +0100

    Adds multiple barkingdog solutions

--- Udemy/Java MasterClass/BarkingDog/src/BarkingDog.java
+++ Udemy/Java MasterClass/BarkingDog/src/BarkingDog.java
@@ -1,15 +1,60 @@
 public class BarkingDog
 {
-
-    public BarkingDog() {}
-
     public static boolean shouldWakeUp(boolean barking, int hourOfDay)
     {
-        if(barking && (hourOfDay < 8 || hourOfDay > 22)
-                && !(hourOfDay < 0 || hourOfDay > 23))
-        {
-            return true;
-        }
-        return false;
+        if(hourOfDay < 0 || hourOfDay > 23) {return false;}
+        return(true ? barking && (hourOfDay < 8 || hourOfDay > 22) : false);
     }
 }
+
+//public class BarkingDog
+//{
+//    public static boolean shouldWakeUp(boolean barking, int hourOfDay)
+//    {
+//        if (!(hourOfDay < 0 || hourOfDay > 23))
+//        {
+//            if(barking && (hourOfDay < 8 || hourOfDay > 22))
+//            {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
+
+
+//public class BarkingDog
+//{
+//    public BarkingDog() {}
+//
+//    public static boolean shouldWakeUp(boolean barking, int hourOfDay)
+//    {
+//        if (!(hourOfDay < 0 || hourOfDay > 23))
+//        {
+//            return(isNight(hourOfDay) && barking);
+//        }
+//        return false;
+//    }
+//
+//    public static boolean isNight(int hour)
+//    {
+//        return((hour < 8 || hour > 22));
+//    }
+//}
+//
+
+//public class BarkingDog
+//{
+//
+//    public BarkingDog() {}
+//
+//    public static boolean shouldWakeUp(boolean barking, int hourOfDay)
+//    {
+//        if(barking && (hourOfDay < 8 || hourOfDay > 22)
+//                && !(hourOfDay < 0 || hourOfDay > 23))
+//        {
+//            return true;
+//        }
+//        return false;
+//    }
+//}
