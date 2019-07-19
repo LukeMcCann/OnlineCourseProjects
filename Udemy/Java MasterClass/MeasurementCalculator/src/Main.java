c5fcbea70358691f94f36784f1d075d8180f95ad
LukeMcCann <developer-lukemccann@outlook.com>
2019-07-19 19:03:34 +0100

    Changes solution to fit unspecified requirement

--- Udemy/Java MasterClass/MeasurementCalculator/src/Main.java
+++ Udemy/Java MasterClass/MeasurementCalculator/src/Main.java
@@ -4,17 +4,26 @@ public class Main
     {
         MeasurementCalculator mc = new MeasurementCalculator();
 
-        System.out.println(mc.calcFeetandInchesToCentimeters(1,2));
+        System.out.println("");
+
         System.out.println(mc.calcFeetandInchesToCentimeters(-1,2));
-        System.out.println(mc.calcFeetandInchesToCentimeters(0,12));
+        System.out.println(mc.calcFeetandInchesToCentimeters(-1,13));
+        System.out.println(mc.calcFeetandInchesToCentimeters(-5,13));
+        System.out.println(mc.calcFeetandInchesToCentimeters(0,22));
         System.out.println(mc.calcFeetandInchesToCentimeters(4,13));
+        System.out.println(mc.calcFeetandInchesToCentimeters(-12));
+        System.out.println(mc.calcFeetandInchesToCentimeters(-1));
+        System.out.println(mc.calcFeetandInchesToCentimeters(-2));
 
         System.out.println("");
 
+        System.out.println(mc.calcFeetandInchesToCentimeters(1,2));
+        System.out.println(mc.calcFeetandInchesToCentimeters(0,12));
         System.out.println(mc.calcFeetandInchesToCentimeters(12));
         System.out.println(mc.calcFeetandInchesToCentimeters(2));
         System.out.println(mc.calcFeetandInchesToCentimeters(13));
         System.out.println(mc.calcFeetandInchesToCentimeters(70));
+        System.out.println(mc.calcFeetandInchesToCentimeters(100));
 
     }
 }
